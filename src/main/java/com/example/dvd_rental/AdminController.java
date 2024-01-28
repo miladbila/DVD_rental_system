84db2061537d6b944f905420d591b72aba72ca54
Milad <milad.tavakoli81@gmail.com>
2024-01-28 19:27:56 +0330

    complete admin buttons methods

--- src/main/java/com/example/dvd_rental/AdminController.java
+++ src/main/java/com/example/dvd_rental/AdminController.java
@@ -54,4 +54,107 @@ public class AdminController {
         stage.setScene(new Scene(root1));
         stage.show();
     }
+
+    public void onFilmInfo(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.FilmInfo.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onActiveRent(ActionEvent event) {
+        AdminActiveRent.staffId = staffId;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.ActiveRents.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onReserverequests(ActionEvent event) {
+        AdminReserveRequests.staffId = staffId;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.reserveRequests.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onBestselling(ActionEvent event) {
+        adminBestSelling.AdminBestSelling.staffId = staffId;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.bestSelling.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onStoreinfo(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.storeInfo.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onFilmlists(ActionEvent event) {
+        AdminFilmList.staffId = staffId;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.filmList.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void onSearchinfilms(ActionEvent event) {
+        AdminSearch.staffId = staffId;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin.search.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 }
---
+++ src/main/java/com/example/dvd_rental/AdminFilmList.java
@@ -0,0 +1,47 @@
+package com.example.dvd_rental;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminFilmList {
+    static String staffId;
+
+    @FXML
+    private TableView tableview;
+
+    @FXML
+    private Label welcome_label;
+    public void initialize() {
+        ResultSet rs = null;
+        try {
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            rs = Database.sqlCommand("select * from film natural join film_category as X  natural join category group by category_id having rating = (select max(rating) from film natural join category where category_id = X.category_id)");
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+                tableview.getColumns().addAll(col);
+            }
+            while (rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    row.add(rs.getString(i));
+                }
+                data.add(row);
+            }
+            tableview.setItems(data);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
