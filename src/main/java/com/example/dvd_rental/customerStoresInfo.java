e1864ec7fb940fb5ae7e5807c956f11826237a1c
zahra kazemi <zahra.k199002@gmail.com>
2024-01-29 01:57:10 +0330

    writing customer StoresInfo function

---
+++ src/main/java/com/example/dvd_rental/customerStoresInfo.java
@@ -0,0 +1,46 @@
+package com.example.dvd_rental;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class CustomerStoresInfo {
+
+    @FXML
+    private Label lbllist;
+
+    @FXML
+    private TableView tableview;
+    public void initialize() {
+        ResultSet rs = null;
+        try {
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            rs = Database.sqlCommand(String.format("select store.store_id , address.address from stores , address  where store.address_id = address.address_id)"));
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+                tableview.getColumns().addAll(col);
+            }
+            while (rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    row.add(rs.getString(i));
+                }
+                data.add(row);
+            }
+            tableview.setItems(data);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
