a0dc27c8506dc37c55c79a0e64c4c8ebd15e0411
zahra kazemi <zahra.k199002@gmail.com>
2024-01-29 22:23:32 +0330

    writing customer edit profile function

---
+++ src/main/java/com/example/dvd_rental/CustomerEditProfile.java
@@ -0,0 +1,64 @@
+package com.example.dvd_rental;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+
+public class CustomerEditProfile {
+    static String customerId;
+
+    @FXML
+    private Button btnAddress;
+
+    @FXML
+    private Button btnEmail;
+
+    @FXML
+    private Button btnFirstName;
+
+    @FXML
+    private Button btnLastName;
+
+    @FXML
+    private TextField txtNew;
+
+    @FXML
+    void onBtnAddress(ActionEvent event) {
+        try {
+            Database.sqlCommand(String.format("update address set address = %s where address.address_id = customer.address_id and customer_id= %s", txtNew.getText() , customerId));
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void onBtnEmail(ActionEvent event) {
+        try {
+            Database.sqlCommand(String.format("update customer set email = %s where customer_id = %s", txtNew.getText() , customerId));
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void onBtnFirstName(ActionEvent event) {
+        try {
+            Database.sqlCommand(String.format("update customer set first_name = %s where where customer_id = %s", txtNew.getText(), customerId));
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void onBtnLastName(ActionEvent event) {
+        try {
+            Database.sqlCommand(String.format("update customer set last_name = %s where customer_id = %s", txtNew.getText() , customerId));
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
