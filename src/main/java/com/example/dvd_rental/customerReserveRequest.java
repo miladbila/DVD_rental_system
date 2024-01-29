bd37153bba5367bccb1cce55f1e43abf2db647ae
zahra kazemi <zahra.k199002@gmail.com>
2024-01-29 17:58:44 +0330

    writing customer reserve request function

---
+++ src/main/java/com/example/dvd_rental/customerReserveRequest.java
@@ -0,0 +1,29 @@
+package com.example.dvd_rental;
+
+//import com.gluonhq.charm.glisten.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+class CustomerReserveRequest {
+
+    @FXML
+    private Button btndone;
+
+    @FXML
+    private Label lbllist;
+
+    @FXML
+    private TextField ttxfilmId;
+
+    @FXML
+    private TextField txtFilmId;
+
+    @FXML
+    void onDone(ActionEvent event) {
+
+    }
+
+}
